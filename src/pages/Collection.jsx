0a84b15c327b67ddbd389c325e1b75c04f02888f
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-01 09:39:38 -0400

    Fix Grid gaps

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,58 +1,17 @@
 import React from 'react'
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import { makeStyles } from '@material-ui/core/styles';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 // components
 import FiltersToolbar from '../components/FiltersToolbar/FiltersToolbar'
+import CollectionGrid from '../components/CollectionGrid/CollectionGrid'
 // assets
-import { collectionItems } from '../data/collection';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-
+import { collectionItems } from '../data/collection'
 
 const Collection = () => {
-
-  const classes = useStyles();
-
-  return (
-    <>
-      <FiltersToolbar />
-
-      <div className={classes.gridContainer}>
-        <GridList className={classes.gridList} cellHeight="auto" cols={3}>
-          {collectionItems.map((product) => (
-            <GridListTile key={product.img}>
-              <LazyLoadImage 
-                className={classes.image} 
-                src={product.img} 
-                alt={product.name}
-                useIntersectionObserver={true}
-                effect="blur"
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-
-    </>
-  )
+	return (
+		<>
+			<FiltersToolbar />
+			<CollectionGrid products={collectionItems} />
+		</>
+	)
 }
-
-const useStyles = makeStyles((theme) => ({
-  gridContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-  },
-  gridList: {
-    margin: '0 !important'
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-  }
-}));
 
 export default Collection
