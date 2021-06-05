9c23dd1914980e1988229a68265a35f321788260
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-04 23:11:37 -0400

    UPDATE - Home all builded with CSS-in-JS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
 import { makeStyles } from '@material-ui/core/styles'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
@@ -10,31 +11,21 @@ import Fade from 'react-reveal/Fade'
 import menImg from '../../assets/images/home/men.jpg'
 import womenImg from '../../assets/images/home/women.jpg'
 import logo from '../../assets/images/isotype_white.png'
-import styles from './Home.module.scss'
 
-const CardMenu = ({ image, links }) => {
+const HeroCard = ({ image, links }) => {
 	const classes = useStyles()
-	const [showLinks, setShowLinks] = useState(false)
 
-	const handleMouseEnter = () => {
-		setShowLinks(true)
-	}
-	const handleMouseLeave = () => {
-		setShowLinks(false)
-	}
 	return (
-		<Box
-			className={styles.hero}
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}>
-			<LazyLoadImage src={image} className={styles.heroImg} />
-			<Box className={`${classes.menuContainer} ${styles.menuContainer}`}>
-				{showLinks &&
-					links.map(linkItem => (
-						<NavLink className={classes.linkCollection} to={linkItem.link}>
+		<Box className={classes.hero}>
+			<LazyLoadImage src={image} className={classes.heroImg} />
+			<Box className={classes.heroLinkContainer}>
+				{links.map(linkItem => (
+					<NavLink className={classes.heroLink} to={linkItem.link}>
+						<Typography component="h2" variant="h3">
 							{linkItem.title}
-						</NavLink>
-					))}
+						</Typography>
+					</NavLink>
+				))}
 			</Box>
 		</Box>
 	)
@@ -50,22 +41,21 @@ const Home = () => {
 		<Box className={classes.root}>
 			<Grid container className={classes.container} spacing={0}>
 				<Grid className={classes.column} item xs={12} md={6}>
-					<CardMenu image={menImg} links={menLinks} />
+					<HeroCard image={menImg} links={menLinks} />
 				</Grid>
 				<Grid className={classes.column} item xs={12} md={6}>
-					<CardMenu image={womenImg} links={womenLinks} />
+					<HeroCard image={womenImg} links={womenLinks} />
 				</Grid>
 			</Grid>
 			<Box className={classes.logoContainer}>
-				<NavLink to="/">
-					<img src={logo} className={classes.logoImg} alt="logotype" />
-				</NavLink>
+				<img src={logo} className={classes.logoImg} alt="logotype" />
 			</Box>
 		</Box>
 	)
 }
 
 const useStyles = makeStyles(theme => ({
+	// Home
 	root: {
 		position: 'relative',
 	},
@@ -74,30 +64,6 @@ const useStyles = makeStyles(theme => ({
 	},
 	column: {
 		height: '100%',
-	},
-	// CardMenu
-	menuContainer: {
-		position: 'absolute',
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'space-around',
-		width: '100%',
-		height: '100%',
-		zIndex: 2,
-		// opacity: 0,
-		transition: 'all ease 0.5s',
-	},
-	linkCollection: {
-		// padding: '5px 8px',
-		color: 'white',
-		fontFamily: theme.typography.h6.fontFamily,
-		fontSize: theme.typography.h4.fontSize,
-		textDecoration: 'none',
-		textTransform: 'uppercase',
-		'&:hover': {
-			textDecoration: 'underline',
-		},
 	},
 	// Logo
 	logoContainer: {
@@ -109,6 +75,70 @@ const useStyles = makeStyles(theme => ({
 	},
 	logoImg: {
 		maxWidth: '300px',
+		userSelect: 'none',
+		userDrag: 'none',
+	},
+	// CardMenu
+	hero: {
+		position: 'relative',
+		height: '100%',
+		overflow: 'hidden',
+		userSelect: 'none',
+		'&:before': {
+			content: "''",
+			width: '100%',
+			height: '100%',
+			position: 'absolute',
+			top: '0',
+			left: '0',
+			backgroundColor: '#000',
+			opacity: 0,
+			transition: 'all ease 0.8s',
+			zIndex: 1,
+		},
+		'&:hover': {
+			'& img': { transform: 'scale(1.08)' },
+			'&:before': { opacity: 0.5 },
+			'& $heroLinkContainer': {
+				opacity: 1,
+			},
+		},
+	},
+	heroImg: {
+		position: 'absolute',
+		top: '0',
+		right: '0',
+		width: '100%',
+		height: '100%',
+		objectFit: 'cover',
+		transition: 'all ease 1.2s',
+	},
+	heroLinkContainer: {
+		position: 'absolute',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		width: '100%',
+		height: '100%',
+		zIndex: 2,
+		opacity: 0,
+		transition: 'all ease 0.38s',
+	},
+	heroLink: {
+		color: 'white',
+		textDecoration: 'none',
+		'&:hover': {
+			textDecoration: 'underline',
+			textUnderlineOffset: '3px',
+			textDecorationThickness: 'from-font',
+		},
+		'& h2': {
+			fontFamily: theme.typography.h6.fontFamily,
+			fontSize: theme.typography.h3.fontSize,
+			fontWeight: 700,
+			textTransform: 'uppercase',
+		},
 	},
 }))
 
