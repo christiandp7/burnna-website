837494dc56d321d9620773d4d8d02ff95d420933
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-05 10:11:24 -0400

    FIX - fix module not found .sass in build time

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 // Styles
 import 'react-lazy-load-image-component/src/effects/blur.css'
 // swiper styles
-import 'swiper/swiper.scss'
+import 'swiper/swiper.min.css'
 import 'swiper/components/effect-fade/effect-fade.min.css'
 import 'swiper/components/navigation/navigation.min.css'
 import 'swiper/components/pagination/pagination.min.css'
