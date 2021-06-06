c78edb6096ed8e8d64eecb9543e2e289f687d94b
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-05 23:25:53 -0400

    ADD - Explore grid

--- src/data/explore.js
+++ src/data/explore.js
@@ -1,88 +1,34 @@
-import swimImg1 from '../assets/images/collection/swim_1.jpg'
-import swimImg2 from '../assets/images/collection/swim_2.jpg'
-import swimImg3 from '../assets/images/collection/swim_3.jpg'
-import swimImg4 from '../assets/images/collection/swim_4.jpg'
-import swimImg5 from '../assets/images/collection/swim_5.jpg'
-import swimImg6 from '../assets/images/collection/swim_6.jpg'
+import exploreImg1 from '../assets/images/explore/explore-1.jpg'
+import exploreImg2 from '../assets/images/explore/explore-2.jpg'
+import exploreImg3 from '../assets/images/explore/explore-3.jpg'
+import exploreImg4 from '../assets/images/explore/explore-4.jpg'
+import exploreImg5 from '../assets/images/explore/explore-5.jpg'
 
-export const collectionItems = [
+export const exploreItems = [
 	{
-		name: 'Swim 1',
-		price: '$150',
-		img: swimImg1,
-		variants: {
-			color: [
-				{ name: 'rose', hex: '#C39797' },
-				{ name: 'blue', hex: '#2B52BA' },
-				{ name: 'black', hex: '#0D0D0D' },
-				{ name: 'brown', hex: '#5F2727' },
-			],
-			size: [],
-		},
+		caption: 'art',
+		img: exploreImg1,
+		alt: 'explore',
 	},
 	{
-		name: 'Swim 2',
-		price: '$150',
-		img: swimImg2,
-		variants: {
-			color: [
-				{ name: 'rose', hex: '#C39797' },
-				{ name: 'ywllow', hex: '#E3AE4E' },
-				{ name: 'black', hex: '#0D0D0D' },
-			],
-			size: [],
-		},
+		caption: 'art',
+		img: exploreImg2,
+		alt: 'explore',
 	},
 	{
-		name: 'Swim 3',
-		price: '$150',
-		img: swimImg3,
-		variants: {
-			color: [
-				{ name: 'rose', hex: '#AE2234' },
-				{ name: 'black', hex: '#0D0D0D' },
-				{ name: 'brown', hex: '#5F2727' },
-			],
-			size: [],
-		},
+		caption: 'art',
+		img: exploreImg3,
+		alt: 'explore',
 	},
 	{
-		name: 'Swim 4',
-		price: '$150',
-		img: swimImg4,
-		variants: {
-			color: [
-				{ name: 'black', hex: '#0D0D0D' },
-				{ name: 'brown', hex: '#5F2727' },
-			],
-			size: [],
-		},
+		caption: 'art',
+		img: exploreImg4,
+		alt: 'explore',
 	},
 	{
-		name: 'Swim 5',
-		price: '$150',
-		img: swimImg5,
-		variants: {
-			color: [
-				{ name: 'blue', hex: '#2B52BA' },
-				{ name: 'brown', hex: '#5F2727' },
-				{ name: 'rose', hex: '#AE2234' },
-				{ name: 'black', hex: '#0D0D0D' },
-			],
-			size: [],
-		},
-	},
-	{
-		name: 'Swim 6',
-		price: '$150',
-		img: swimImg6,
-		variants: {
-			color: [
-				{ name: 'rose', hex: '#C39797' },
-				{ name: 'black', hex: '#0D0D0D' },
-				{ name: 'blue', hex: '#2B52BA' },
-			],
-			size: [],
-		},
+		caption: 'art',
+		img: exploreImg5,
+		alt: 'explore',
+		cols: 8,
 	},
 ]
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import MainLayout from '../layouts/MainLayout'
+// data
+import { exploreItems } from '../data/explore'
 
 const Explore = () => {
+	const classes = useStyles()
 	return (
 		<MainLayout>
-			<GridList>
-				<GridListTile>
-					<LazyLoadImage
-						// wrapperClassName={classes.lazyLoadWrapper}
-						// className={classes.image}
-						src={explore.img}
-						alt={explore.name}
-						useIntersectionObserver={true}
-						effect="blur"
-					/>
-				</GridListTile>
-			</GridList>
+			<Grid container>
+				{exploreItems.map(exploreItem => (
+					<Grid item xs sm={exploreItem.cols || 4}>
+						<LazyLoadImage
+							wrapperClassName={classes.lazyLoadWrapper}
+							className={classes.image}
+							src={exploreItem.img}
+							alt={exploreItem.alt}
+							useIntersectionObserver={true}
+							effect="blur"
+						/>
+					</Grid>
+				))}
+			</Grid>
 		</MainLayout>
 	)
 }
+
+const useStyles = makeStyles(theme => ({
+	lazyLoadWrapper: {
+		height: '100%',
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+		objectFit: 'cover',
+	},
+}))
 
 export default Explore
