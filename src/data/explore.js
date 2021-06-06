6e2f7cfe57b4f4ac7eeb8116ce32fa1a42948995
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-05 23:01:15 -0400

    ADD - ready to start working on Explore page

---
+++ src/data/explore.js
@@ -0,0 +1,88 @@
+import swimImg1 from '../assets/images/collection/swim_1.jpg'
+import swimImg2 from '../assets/images/collection/swim_2.jpg'
+import swimImg3 from '../assets/images/collection/swim_3.jpg'
+import swimImg4 from '../assets/images/collection/swim_4.jpg'
+import swimImg5 from '../assets/images/collection/swim_5.jpg'
+import swimImg6 from '../assets/images/collection/swim_6.jpg'
+
+export const collectionItems = [
+	{
+		name: 'Swim 1',
+		price: '$150',
+		img: swimImg1,
+		variants: {
+			color: [
+				{ name: 'rose', hex: '#C39797' },
+				{ name: 'blue', hex: '#2B52BA' },
+				{ name: 'black', hex: '#0D0D0D' },
+				{ name: 'brown', hex: '#5F2727' },
+			],
+			size: [],
+		},
+	},
+	{
+		name: 'Swim 2',
+		price: '$150',
+		img: swimImg2,
+		variants: {
+			color: [
+				{ name: 'rose', hex: '#C39797' },
+				{ name: 'ywllow', hex: '#E3AE4E' },
+				{ name: 'black', hex: '#0D0D0D' },
+			],
+			size: [],
+		},
+	},
+	{
+		name: 'Swim 3',
+		price: '$150',
+		img: swimImg3,
+		variants: {
+			color: [
+				{ name: 'rose', hex: '#AE2234' },
+				{ name: 'black', hex: '#0D0D0D' },
+				{ name: 'brown', hex: '#5F2727' },
+			],
+			size: [],
+		},
+	},
+	{
+		name: 'Swim 4',
+		price: '$150',
+		img: swimImg4,
+		variants: {
+			color: [
+				{ name: 'black', hex: '#0D0D0D' },
+				{ name: 'brown', hex: '#5F2727' },
+			],
+			size: [],
+		},
+	},
+	{
+		name: 'Swim 5',
+		price: '$150',
+		img: swimImg5,
+		variants: {
+			color: [
+				{ name: 'blue', hex: '#2B52BA' },
+				{ name: 'brown', hex: '#5F2727' },
+				{ name: 'rose', hex: '#AE2234' },
+				{ name: 'black', hex: '#0D0D0D' },
+			],
+			size: [],
+		},
+	},
+	{
+		name: 'Swim 6',
+		price: '$150',
+		img: swimImg6,
+		variants: {
+			color: [
+				{ name: 'rose', hex: '#C39797' },
+				{ name: 'black', hex: '#0D0D0D' },
+				{ name: 'blue', hex: '#2B52BA' },
+			],
+			size: [],
+		},
+	},
+]
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
 import MainLayout from '../layouts/MainLayout'
 
 const Explore = () => {
-	return <MainLayout>Explore</MainLayout>
+	return (
+		<MainLayout>
+			<GridList>
+				<GridListTile>
+					<LazyLoadImage
+						// wrapperClassName={classes.lazyLoadWrapper}
+						// className={classes.image}
+						src={explore.img}
+						alt={explore.name}
+						useIntersectionObserver={true}
+						effect="blur"
+					/>
+				</GridListTile>
+			</GridList>
+		</MainLayout>
+	)
 }
 
 export default Explore
