831e912b36d38c53b421b7049085672da1a50233
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-06 10:43:43 -0400

    ADD - contact image placed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+// import Grid from '@material-ui/core/Grid'
+
+import { LazyLoadImage } from 'react-lazy-load-image-component' // import Grid from '@material-ui/core/Grid'
+// components
 import MainLayout from '../layouts/MainLayout'
+// assets
+import contactImg from '../assets/images/contact/contact-img.jpg'
 
 const Contact = () => {
-	return <MainLayout>Contact</MainLayout>
+	const classes = useStyles()
+	return (
+		<MainLayout>
+			<Grid container>
+				<Grid item xs>
+					<LazyLoadImage
+						className={classes.image}
+						src={contactImg}
+						alt="contact img"
+						useIntersectionObserver={true}
+						effect="blur"
+					/>
+				</Grid>
+				<Grid item xs></Grid>
+			</Grid>
+		</MainLayout>
+	)
 }
+
+const useStyles = makeStyles(theme => ({
+	image: {
+		width: '100%',
+	},
+}))
 
 export default Contact
