0cce5e78324525e83c46d30540e0b68bfcbba895
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-09 15:32:46 -0400

    Start Return Policy page develop

--- src/pages/FAQ/ReturnPolicy.jsx
+++ src/pages/FAQ/ReturnPolicy.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
 import FAQLayout from '../../layouts/FAQLayout'
 
 const ReturnPolicy = () => {
-	return <FAQLayout>Return</FAQLayout>
+	return (
+		<FAQLayout>
+			<Container>return</Container>
+		</FAQLayout>
+	)
 }
 
 export default ReturnPolicy
