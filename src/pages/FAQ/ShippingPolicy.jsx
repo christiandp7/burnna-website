88efb44135e6907132228874724cd6a2f5aaf547
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-10 11:47:30 -0400

    DONE - Shipping Policy page done

--- src/pages/FAQ/ShippingPolicy.jsx
+++ src/pages/FAQ/ShippingPolicy.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
 import FAQLayout from '../../layouts/FAQLayout'
+import FAQAccordion from '../../components/Accordion/FAQAccordion'
+import Heading from '../../components/Typography/Heading'
+import Typography from '@material-ui/core/Typography'
+// data
+import shippingPolicyFaqs from '../../data/shippingPolicy'
 
 const ShippingPolicy = () => {
-	return <FAQLayout>Shipping</FAQLayout>
+	return (
+		<FAQLayout top="50px" mobTop="30px" alignItemsCenter>
+			<Container>
+				<Heading textTransform="initial">Shipping policy</Heading>
+				<FAQAccordion faqs={shippingPolicyFaqs} />
+			</Container>
+		</FAQLayout>
+	)
 }
 
 export default ShippingPolicy
