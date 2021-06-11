645a47616d7300cf925754405c7ee954ba5e2856
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-10 23:48:37 -0400

    ADD - Tabs, need to refactor

--- src/components/SizeGuideSidebar/SizeGuideSidebarContent.jsx
+++ src/components/SizeGuideSidebar/SizeGuideSidebarContent.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+function TabPanel(props) {
+	const { children, value, index, ...other } = props
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`full-width-tabpanel-${index}`}
+			aria-labelledby={`full-width-tab-${index}`}
+			{...other}>
+			{value === index && (
+				<Box p={3}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	)
+}
 
 const SizeGuideSidebarContent = () => {
 	const classes = useStyles()
-	return <div className={classes.root}>SizeGuide</div>
+	const [value, setValue] = React.useState(0)
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue)
+	}
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					aria-label="simple tabs example"
+					centered>
+					<Tab label="Women Swinms" />
+					<Tab label="Men Swimns" />
+				</Tabs>
+			</AppBar>
+			<TabPanel value={value} index={0}>
+				Item One
+			</TabPanel>
+			<TabPanel value={value} index={1}>
+				Item Two
+			</TabPanel>
+		</div>
+	)
 }
 
 const useStyles = makeStyles(theme => ({
@@ -11,6 +60,7 @@ const useStyles = makeStyles(theme => ({
 		height: '100%',
 		color: theme.palette.primary.contrastText,
 		cursor: 'default',
+		padding: `${theme.spacing(3)}px ${theme.spacing(5)}px`,
 	},
 }))
 
