25a7dbc5bf9bba424c0a567d30fa5065cd984699
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-12 23:02:17 -0400

    ADD - setup context to control drawers state

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ import ProductCare from './pages/FAQ/ProductCare'
 import Payment from './pages/FAQ/Payment'
 import SizeGuide from './pages/FAQ/SizeGuide'
 import NotFound from './pages/NotFound'
+// components
+import { DrawerContextProvider } from './context/DrawerContext'
 // data
 import theme from './theme/theme'
 
@@ -35,29 +37,35 @@ SwiperCore.use([EffectFade, Navigation, Pagination])
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Router>
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/collection" component={Collection} />
-					<Route exact path="/product" component={Product} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/explore" component={Explore} />
-					<Route exact path="/contact" component={Contact} />
-					<Route exact path="/return-policy" component={ReturnPolicy} />
-					<Route
-						exact
-						path="/shipping-policy"
-						component={ShippingPolicy}
-					/>
-					<Route exact path="/product-care" component={ProductCare} />
-					<Route exact path="/payment" component={Payment} />
-					<Route exact path="/size-guide" component={SizeGuide} />
-					{/* <Route exact path="/faq" component={FAQ} /> */}
-					<Route exact path="/404" component={NotFound} />
-					<Redirect from="*" to="/404" />
-				</Switch>
-			</Router>
+			<DrawerContextProvider>
+				<CssBaseline />
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/collection" component={Collection} />
+						<Route exact path="/product" component={Product} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/explore" component={Explore} />
+						<Route exact path="/contact" component={Contact} />
+						<Route
+							exact
+							path="/return-policy"
+							component={ReturnPolicy}
+						/>
+						<Route
+							exact
+							path="/shipping-policy"
+							component={ShippingPolicy}
+						/>
+						<Route exact path="/product-care" component={ProductCare} />
+						<Route exact path="/payment" component={Payment} />
+						<Route exact path="/size-guide" component={SizeGuide} />
+						{/* <Route exact path="/faq" component={FAQ} /> */}
+						<Route exact path="/404" component={NotFound} />
+						<Redirect from="*" to="/404" />
+					</Switch>
+				</Router>
+			</DrawerContextProvider>
 		</ThemeProvider>
 	)
 }
