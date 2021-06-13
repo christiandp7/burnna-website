22cf55f45cd72b2cd6abeaba6383e599d53069c6
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-12 21:38:31 -0400

    REFACTOR - LinkButton using withStyles instead

--- src/components/Button/LinkButton.jsx
+++ src/components/Button/LinkButton.jsx
@@ -2,40 +2,8 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
-// const LinkButton = props => {
-// 	const classes = useStyles()
-// 	return (
-// 		<Button
-// 			{...props}
-// 			className={classes.root}
-// 			variant="text"
-// 			disableRipple={true}
-// 			underline="always">
-// 			{props.children}
-// 		</Button>
-// 	)
-// }
-
-// const useStyles = makeStyles(theme => ({
-// 	root: {
-// 		// fontSize: theme.typography.h5.fontSize,
-// 		fontWeight: theme.typography.fontWeightRegular,
-// 		textTransform: 'none',
-// 		textDecoration: 'underline',
-// 		textDecorationThickness: 'from-font',
-// 		textUnderlineOffset: '2px',
-// 		'&:hover': {
-// 			textDecoration: 'underline',
-// 			textDecorationThickness: '2px',
-// 			textUnderlineOffset: '2px',
-// 			backgroundColor: 'transparent',
-// 		},
-// 	},
-// }))
-
 const LinkButton = withStyles(theme => ({
 	root: {
-		// fontSize: theme.typography.h5.fontSize,
 		fontWeight: theme.typography.fontWeightRegular,
 		textTransform: 'none',
 		textDecoration: 'underline',
