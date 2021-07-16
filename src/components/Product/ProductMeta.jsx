1327cb5c3a5b9a49fd53fd07f767cf108dc7d3f6
Christian Diaz Portela <christiandp7@gmail.com>
2021-07-16 16:59:53 -0400

    :heavy_minus_sign: Remove react-sticky and Install react-sticky-el

--- src/components/Product/ProductMeta.jsx
+++ src/components/Product/ProductMeta.jsx
@@ -12,6 +12,8 @@ import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
 import LinkButton from '../Button/LinkButton'
 import DrawerContext from '../../context/DrawerContext'
+// import { StickyContainer, Sticky } from 'react-sticky'
+import Sticky from 'react-sticky-el'
 
 const ProductMeta = () => {
 	const classes = useStyles()
@@ -19,72 +21,82 @@ const ProductMeta = () => {
 	const { setCartOpen, setSizeGuideOpen } = useContext(DrawerContext)
 
 	return (
-		<div className={classes.root}>
-			<div className={classes.wrapper}>
-				<div className={classes.productHeading}>
-					<Typography
-						component="h2"
-						variant="h3"
-						className={classes.productTitle}>
-						One Piece Laura
-					</Typography>
-					{/* <Typography
+		<div className={`${classes.slugContainer} slugContainer`}>
+			<Sticky boundaryElement=".slugContainer">
+				<div className={classes.rootWrapper}>
+					<div className={classes.root}>
+						<div className={classes.wrapper}>
+							<div className={classes.productHeading}>
+								<Typography
+									component="h2"
+									variant="h3"
+									className={classes.productTitle}>
+									One Piece Laura
+								</Typography>
+								{/* <Typography
 						component="h3"
 						variant="h4"
 						className={classes.productSubtitle}>
 						One Piece
 					</Typography> */}
-				</div>
-				<div className={classes.variantsContainer}>
-					<div className={classes.variant}>
-						<Typography component="h5" variant="h5">
-							Color
-						</Typography>
-						<div
-							className={`${classes.selectorWrapper} selectorColorWrapper`}>
-							<ColorSelectorButtonGroup />
+							</div>
+							<div className={classes.variantsContainer}>
+								<div className={classes.variant}>
+									<Typography component="h5" variant="h5">
+										Color
+									</Typography>
+									<div
+										className={`${classes.selectorWrapper} selectorColorWrapper`}>
+										<ColorSelectorButtonGroup />
+									</div>
+								</div>
+								<div className={classes.variant}>
+									<Box
+										display="flex"
+										justifyContent="space-between"
+										alignItems="center">
+										<Typography component="h5" variant="h5">
+											Size
+										</Typography>
+										<LinkButton
+											className={classes.sizeGuideButton}
+											onClick={() => setSizeGuideOpen(true)}>
+											Size guide
+										</LinkButton>
+									</Box>
+									<div className={classes.selectorWrapper}>
+										<SizeSelectorButtonGroup />
+									</div>
+								</div>
+							</div>
+							<div className={classes.addToCartContainer}>
+								<AddToCartButton onClick={() => setCartOpen(true)}>
+									<Grid container justify="space-between">
+										<Grid item>
+											<Typography variant="h5">Add</Typography>
+										</Grid>
+										<Grid item>
+											<Typography variant="h5">$150</Typography>
+										</Grid>
+									</Grid>
+								</AddToCartButton>
+							</div>
+						</div>
+						<div className={classes.accordionContainer}>
+							<CustomAccordion />
 						</div>
 					</div>
-					<div className={classes.variant}>
-						<Box
-							display="flex"
-							justifyContent="space-between"
-							alignItems="center">
-							<Typography component="h5" variant="h5">
-								Size
-							</Typography>
-							<LinkButton
-								className={classes.sizeGuideButton}
-								onClick={() => setSizeGuideOpen(true)}>
-								Size guide
-							</LinkButton>
-						</Box>
-						<div className={classes.selectorWrapper}>
-							<SizeSelectorButtonGroup />
-						</div>
-					</div>
 				</div>
-				<div className={classes.addToCartContainer}>
-					<AddToCartButton onClick={() => setCartOpen(true)}>
-						<Grid container justify="space-between">
-							<Grid item>
-								<Typography variant="h5">Add</Typography>
-							</Grid>
-							<Grid item>
-								<Typography variant="h5">$150</Typography>
-							</Grid>
-						</Grid>
-					</AddToCartButton>
-				</div>
-			</div>
-			<div className={classes.accordionContainer}>
-				<CustomAccordion />
-			</div>
+			</Sticky>
 		</div>
 	)
 }
 
 const useStyles = makeStyles(theme => ({
+	slugContainer: {
+		height: '100%',
+	},
+	rootWrapper: {},
 	root: {
 		padding: `${theme.spacing(4)}px ${theme.spacing(2)}px`,
 		textTransform: 'uppercase',
