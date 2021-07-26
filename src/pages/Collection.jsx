0f90c11fda3307809c98e030066c57c860abad5e
Christian Diaz Portela <christiandp7@gmail.com>
2021-07-26 15:48:15 -0400

    :sparkles: Add logo over collectionGrid

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 // Layout
 import MainLayout from '../layouts/MainLayout'
 // components
-import FiltersToolbar from '../components/FiltersToolbar/FiltersToolbar'
+import LogoText from '../assets/svg/LogoText'
 import CollectionGrid from '../components/CollectionGrid/CollectionGrid'
 // assets
 import { collectionItems } from '../data/collection'
 
 const Collection = () => {
+	const classes = useStyles()
 	return (
 		<MainLayout>
-			{/* <FiltersToolbar /> */}
-			<CollectionGrid products={collectionItems} />
+			<div className={classes.collectionWrapper}>
+				<div className={classes.logoContainer}>
+					<LogoText className={classes.logo} />
+				</div>
+				<CollectionGrid products={collectionItems} />
+			</div>
 		</MainLayout>
 	)
 }
+
+const useStyles = makeStyles(theme => ({
+	collectionWrapper: {
+		position: 'relative',
+	},
+	logoContainer: {
+		position: 'absolute',
+		width: '100%',
+		top: 0,
+		textAlign: 'center',
+		paddingTop: theme.spacing(3),
+		zIndex: 1,
+		[theme.breakpoints.down('md')]: {
+			display: 'none',
+		},
+	},
+	logo: {
+		width: '200px',
+	},
+}))
 
 export default Collection
